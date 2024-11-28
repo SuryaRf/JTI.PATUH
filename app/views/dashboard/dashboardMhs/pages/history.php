9c95613bf1e740e91a3e34ea09f149018001ddf7
sittafatim4 <sitta.fatimaa@gmail.com>
2024-11-28 12:10:28 +0700

    aju banding

--- app/views/dashboard/dashboardMhs/pages/history.php
+++ app/views/dashboard/dashboardMhs/pages/history.php
@@ -37,9 +37,28 @@
     }
 
     .table,
-    .btn,
-    .badge,
-    .text-sm,
+    .text-16 {
+      font-family: 'Poppins', sans-serif !important;
+      font-size: 16px;
+    }
+
+    .btn-primary {
+      background-color: #223381;
+      border: none;
+      border-radius: 12px;
+      margin-top: 15px;
+    }
+
+    .badge {
+      font-size: 0.875rem;
+      padding: 0.5em 0.75em;
+    }
+
+    .text-sm {
+      font-size: 16px;
+      font-weight: normal;
+    }
+
     .font-weight-bold,
     .nav-link-text,
     h4,
@@ -61,6 +80,10 @@
 
     .text-black {
       color: #000 !important;
+    }
+
+    .btn-primary:hover {
+      background-color: #1a285e;
     }
 
     .table th,
@@ -181,113 +204,11 @@
             <li class="breadcrumb-item text-sm"><a class="opacity-5 text-white" href="javascript:;">Pages</a></li>
             <li class="breadcrumb-item text-sm text-white active" aria-current="page">Tables</li>
           </ol>
-          <h6 class="font-weight-bolder text-white mb-0">Tables</h6>
+          <h6 class="text-white mb-0">Tables</h6>
         </nav>
         <div class="collapse navbar-collapse mt-sm-0 mt-2 me-md-0 me-sm-4" id="navbar">
           <div class="ms-md-auto pe-md-3 d-flex align-items-center">
-
           </div>
-          <ul class="navbar-nav  justify-content-end">
-            <li class="nav-item d-flex align-items-center">
-              <a href="javascript:;" class="nav-link text-white font-weight-bold px-0">
-                <i class="fa fa-user me-sm-1"></i>
-              </a>
-            </li>
-            <li class="nav-item d-xl-none ps-3 d-flex align-items-center">
-              <a href="javascript:;" class="nav-link text-white p-0" id="iconNavbarSidenav">
-                <div class="sidenav-toggler-inner">
-                  <i class="sidenav-toggler-line bg-white"></i>
-                  <i class="sidenav-toggler-line bg-white"></i>
-                  <i class="sidenav-toggler-line bg-white"></i>
-                </div>
-              </a>
-            </li>
-            <li class="nav-item px-3 d-flex align-items-center">
-              <a href="javascript:;" class="nav-link text-white p-0">
-                <i class="fa fa-cog fixed-plugin-button-nav cursor-pointer"></i>
-              </a>
-            </li>
-            <li class="nav-item dropdown pe-2 d-flex align-items-center">
-              <a href="javascript:;" class="nav-link text-white p-0" id="dropdownMenuButton" data-bs-toggle="dropdown"
-                aria-expanded="false">
-                <i class="fa fa-bell cursor-pointer"></i>
-              </a>
-              <ul class="dropdown-menu  dropdown-menu-end  px-2 py-3 me-sm-n4" aria-labelledby="dropdownMenuButton">
-                <li class="mb-2">
-                  <a class="dropdown-item border-radius-md" href="javascript:;">
-                    <div class="d-flex py-1">
-                      <div class="my-auto">
-                        <img src="../../../../../public/img/team-2.jpg" class="avatar avatar-sm  me-3 ">
-                      </div>
-                      <div class="d-flex flex-column justify-content-center">
-                        <h6 class="text-sm font-weight-normal mb-1">
-                          <span class="font-weight-bold">New message</span> from Laur
-                        </h6>
-                        <p class="text-xs text-secondary mb-0">
-                          <i class="fa fa-clock me-1"></i>
-                          13 minutes ago
-                        </p>
-                      </div>
-                    </div>
-                  </a>
-                </li>
-                <li class="mb-2">
-                  <a class="dropdown-item border-radius-md" href="javascript:;">
-                    <div class="d-flex py-1">
-                      <div class="my-auto">
-                        <img src="../../../../../public/img/small-logos/logo-spotify.svg"
-                          class="avatar avatar-sm bg-gradient-dark  me-3 ">
-                      </div>
-                      <div class="d-flex flex-column justify-content-center">
-                        <h6 class="text-sm font-weight-normal mb-1">
-                          <span class="font-weight-bold">New album</span> by Travis Scott
-                        </h6>
-                        <p class="text-xs text-secondary mb-0">
-                          <i class="fa fa-clock me-1"></i>
-                          1 day
-                        </p>
-                      </div>
-                    </div>
-                  </a>
-                </li>
-                <li>
-                  <a class="dropdown-item border-radius-md" href="javascript:;">
-                    <div class="d-flex py-1">
-                      <div class="avatar avatar-sm bg-gradient-secondary  me-3  my-auto">
-                        <svg width="12px" height="12px" viewBox="0 0 43 36" version="1.1"
-                          xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-                          <title>credit-card</title>
-                          <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-                            <g transform="translate(-2169.000000, -745.000000)" fill="#FFFFFF" fill-rule="nonzero">
-                              <g transform="translate(1716.000000, 291.000000)">
-                                <g transform="translate(453.000000, 454.000000)">
-                                  <path class="color-background"
-                                    d="M43,10.7482083 L43,3.58333333 C43,1.60354167 41.3964583,0 39.4166667,0 L3.58333333,0 C1.60354167,0 0,1.60354167 0,3.58333333 L0,10.7482083 L43,10.7482083 Z"
-                                    opacity="0.593633743"></path>
-                                  <path class="color-background"
-                                    d="M0,16.125 L0,32.25 C0,34.2297917 1.60354167,35.8333333 3.58333333,35.8333333 L39.4166667,35.8333333 C41.3964583,35.8333333 43,34.2297917 43,32.25 L43,16.125 L0,16.125 Z M19.7083333,26.875 L7.16666667,26.875 L7.16666667,23.2916667 L19.7083333,23.2916667 L19.7083333,26.875 Z M35.8333333,26.875 L28.6666667,26.875 L28.6666667,23.2916667 L35.8333333,23.2916667 L35.8333333,26.875 Z">
-                                  </path>
-                                </g>
-                              </g>
-                            </g>
-                          </g>
-                        </svg>
-                      </div>
-                      <div class="d-flex flex-column justify-content-center">
-                        <h6 class="text-sm font-weight-normal mb-1">
-                          Payment successfully completed
-                        </h6>
-                        <p class="text-xs text-secondary mb-0">
-                          <i class="fa fa-clock me-1"></i>
-                          2 days
-                        </p>
-                      </div>
-                    </div>
-                  </a>
-                </li>
-              </ul>
-            </li>
-          </ul>
         </div>
       </div>
     </nav>
@@ -312,41 +233,50 @@
                   </thead>
                   <tbody>
                     <tr>
-                      <td class="text-sm" style="color: #6B7280; text-align: center; font-size: 16px;">ABC01</td>
-                      <td class="text-sm" style="color: #6B7280; font-size: 16px;">Merokok</td>
+                      <td class="text-16" style="color: #6B7280; text-align: center; font-size: 16px;">ABC01</td>
+                      <td class="text-16" style="color: #6B7280; font-size: 16px;">Merokok</td>
                       <td class="text-center">
-                        <span class="badge bg-warning text-white p-2 fs-6 rounded-3"
-                          style="width: 100px; text-align: center;">TERTUNDA</span>
+                        <span class="badge bg-warning text-white fs-7 rounded-3"
+                          style="font-weight: 600; width: 100px; text-align: center; height: 30px; line-height: 18px;">
+                          TERTUNDA
+                        </span>
                       </td>
                       <td class="align-middle text-center">
-                        <button class="btn btn-sm btn-outline-primary">
-                          <i class="fas fa-eye me-1"></i>CHECK
+                        <button class="btn btn-primary py-1 px-4 fs-7 w-45 rounded-3" data-bs-toggle="modal"
+                          data-bs-target="#detailModalABC01" style="font-weight: 600;">
+                          CHECK
                         </button>
                       </td>
                     </tr>
                     <tr>
-                      <td class="text-sm" style="color: #6B7280; text-align: center; font-size: 16px;">DEF02</td>
-                      <td class="text-sm" style="color: #6B7280; font-size: 16px;">Makan di Kelas</td>
+                      <td class="text-16" style="color: #6B7280; text-align: center; font-size: 16px;">DEF02</td>
+                      <td class="text-16" style="color: #6B7280; font-size: 16px;">Makan di Kelas</td>
                       <td class="text-center">
-                        <span class="badge bg-success text-white"
-                          style="display: inline-block; width: 120px; text-align: center; font-size: 16px;">SELESAI</span>
+                        <span class="badge bg-success text-white p-2 fs-7 rounded-3"
+                          style="font-weight: 600; width: 100px; text-align: center; height: 30px; line-height: 16px;">
+                          SELESAI
+                        </span>
                       </td>
-                      <td class="align-middle text-center">
-                        <button class="btn btn-sm btn-outline-success">
-                          <i class="fas fa-check-circle me-1"></i>CHECK
+                      <td class="text-center">
+                        <button class="btn btn-primary py-1 px-4 fs-7 w-45 rounded-3" data-bs-toggle="modal"
+                          data-bs-target="#detailModalABC01" style="font-weight: 600;">
+                          CHECK
                         </button>
                       </td>
                     </tr>
                     <tr>
-                      <td class="text-sm" style="color: #6B7280; text-align: center; font-size: 16px;">GHI03</td>
-                      <td class="text-sm" style="color: #6B7280; font-size: 16px;">Bolos</td>
+                      <td class="text-16" style="color: #6B7280; text-align: center; font-size: 16px;">GHI03</td>
+                      <td class="text-16" style="color: #6B7280; font-size: 16px;">Bolos</td>
                       <td class="text-center">
-                        <span class="badge bg-danger text-white"
-                          style="display: inline-block; width: 120px; text-align: center; font-size: 16px;">DITOLAK</span>
+                        <span class="badge bg-danger text-white p-2 fs-7 rounded-3"
+                          style="font-weight: 600; width: 100px; text-align: center;">
+                          DITOLAK
+                        </span>
                       </td>
-                      <td class="align-middle text-center">
-                        <button class="btn btn-sm btn-outline-danger">
-                          <i class="fas fa-times-circle me-1"></i>CHECK
+                      <td class="text-center">
+                        <button class="btn btn-primary py-1 px-4 fs-7 w-45 rounded-3" data-bs-toggle="modal"
+                          data-bs-target="#detailModalABC01" style="font-weight: 600;">
+                          CHECK
                         </button>
                       </td>
                     </tr>
@@ -386,7 +316,7 @@
                     <!-- Baris 1 -->
                     <tr>
                       <td class="text-sm font-weight-bold text-primary">ABC01</td>
-                      <td class="text-sm">Merokok</td>
+                      <td class="text-sm ">Merokok</td>
                       <td class="text-center">
                         <span class="badge bg-warning text-white">Pending</span>
                       </td>
