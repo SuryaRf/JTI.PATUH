7b7a412ea3d1e4bff0fb52f380002203b4a574d8
sittafatim4 <sitta.fatimaa@gmail.com>
2024-11-29 15:34:42 +0700

    fix history

--- app/controllers/connection.php
+++ app/controllers/connection.php
@@ -1,10 +1,6 @@
 <?php
 // Konfigurasi Koneksi SQL Server
-<<<<<<< HEAD
-$serverName = "DESKTOP-2SC3JDH"; // Sesuaikan dengan nama server Anda
-=======
 $serverName = "DESKTOP-HQU7BU7"; // Sesuaikan dengan nama server Anda
->>>>>>> 6e81e296d6a5ace17358e23ae716fdb64902ae1c
 $database = "PBL";    // Nama database
 
 // Opsi koneksi
